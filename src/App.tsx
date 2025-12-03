17b9d3a2bd3d2bae9df63bc0a264cab0ddf807ca
vanhieuls <vanhieuls1325147@gmail.com>
2025-12-03 10:10:20 +0700

    feature/Add Contact, Lecturer, Lecturer Details and Registration pages with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Homepage from "./pages/Homepage/Homepage";
+import Contact from "./pages/Contact/Contact";
+import Lecturer from "./pages/Lecturer/Lecturer";
+import LecturerDetails from "./pages/Lecturer/LecturerDetails";
+import Registration from "./pages/Lecturer/Registration";
 
 const App = () => {
   return (
@@ -20,6 +24,14 @@ const App = () => {
           {/* <Route element={<ProtectedRoutes />}> */}
           {/* Homepage */}
           <Route path="/" element={<Homepage />} />
+          {/* Contact */}
+          <Route path="/contact" element={<Contact />} />
+          {/* Lecturer */}
+          <Route path="/lecturer" element={<Lecturer />} />
+          {/* Lecturer Details */}
+          <Route path="/lecturer/:id" element={<LecturerDetails />} />
+          {/* Registration */}
+          <Route path="/registration" element={<Registration />} />
         </Route>
         {/* </Route> */}
       </Routes>
