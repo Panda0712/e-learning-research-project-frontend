b008885e393ebff74a2a66109b621ece30fd172e
Pham Tuan <tuan.pham.super@gmail.com>
2026-03-03 07:28:05 +0700

    feature/implement-axios-interceptors

---
+++ src/apis/auth.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { toast } from "react-toastify";
+import authorizedAxiosInstance from "../utils/authorizedAxios";
+import { API_ROOT } from "../utils/constants";
+
+// handle refresh token
+const refreshTokenAPI = async () => {
+  const res = await authorizedAxiosInstance.get(
+    `${API_ROOT}/v1/users/refresh_token`,
+  );
+  return res.data;
+};
+
+// handle register new user
+const registerUserAPI = async (data: any) => {
+  const res = await authorizedAxiosInstance.post(
+    `${API_ROOT}/v1/users/register`,
+    data,
+  );
+  return res.data;
+};
+
+// handle verify new email
+const verifyUserAPI = async (data: any) => {
+  const res = await authorizedAxiosInstance.put(
+    `${API_ROOT}/v1/users/verify`,
+    data,
+  );
+  toast.success(
+    "Verified account successfully!! Please login with your email address!!",
+    { theme: "colored" },
+  );
+  return res.data;
+};
+
+export const authService = {
+  refreshTokenAPI,
+  registerUserAPI,
+  verifyUserAPI,
+};
