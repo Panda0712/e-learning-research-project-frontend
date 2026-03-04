27213c2d836b72f0096ce953a671bb2a87e61448
Pham Tuan <tuan.pham.super@gmail.com>
2026-03-04 07:37:53 +0700

    feature/ptuan: implement oauth login and sign up logic, fix bugs redux store dispatch

--- src/pages/Auth/VerificationPage.tsx
+++ src/pages/Auth/VerificationPage.tsx
@@ -4,26 +4,23 @@ import { authService } from "../../apis/auth";
 import Loading from "../../components/ui/Loading";
 
 const VerificationPage = () => {
-    const [searchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
 
-    const {email, token} = Object.fromEntries([...searchParams]);
+  const { email, token } = Object.fromEntries([...searchParams]);
 
-    const [verified, setVerified] = useState(false);
+  const [verified, setVerified] = useState(false);
 
-    useEffect((
-    )=>{
-         if(email && token){
-            authService.verifyUserAPI({email,token}).then(()=>{
-                setVerified(true);
-            })
-        }
-    },[email, token]);
+  useEffect(() => {
+    if (email && token) {
+      authService.verifyUserAPI({ email, token }).then(() => {
+        setVerified(true);
+      });
+    }
+  }, [email, token]);
 
-    if(!verified) return <Loading caption="We are verifying your email..."/>
+  if (!verified) return <Loading caption="We are verifying your email..." />;
 
-  return (
-    <Navigate to={`/auth/login?verifiedEmail=${email}`}/>
-  )
-}
+  return <Navigate to={`/auth/login?verifiedEmail=${email}`} />;
+};
 
-export default VerificationPage
+export default VerificationPage;
