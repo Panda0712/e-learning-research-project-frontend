fa553b9711cb1138db6d05a8b3e4ebe4bd984ac5
Minh Khoa <khoaminh010704@gmail.com>
2026-03-16 20:58:22 +0700

    feature/minhkhoa-set-up-Dashboard-Admin-Transactions

---
+++ src/apis/transaction.ts
@@ -0,0 +1,14 @@
+import authorizedAxiosInstance from "../utils/authorizedAxios";
+import { API_ROOT } from "../utils/constants";
+
+// Gọi API lấy tất cả giao dịch cho Admin
+const getAllTransactionsAPI = async () => {
+  const res = await authorizedAxiosInstance.get(
+    `${API_ROOT}/v1/transactions/get-all-transactions`
+  );
+  return res.data;
+};
+
+export const transactionService = {
+  getAllTransactionsAPI,
+};
