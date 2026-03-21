db68163b8dd5ed6a6453d6c88560bd164eff0c1d
Phạm Ngũ Tuân <123355012+Panda0712@users.noreply.github.com>
2026-03-21 19:03:02 +0700

    Update transaction.ts

--- src/apis/transaction.ts
+++ src/apis/transaction.ts
@@ -1,7 +1,7 @@
 import authorizedAxiosInstance from "../utils/authorizedAxios";
 import { API_ROOT } from "../utils/constants";
 
-// Gọi API lấy tất cả giao dịch cho Admin
+// Get all transactions API for admin
 const getAllTransactionsAPI = async () => {
   const res = await authorizedAxiosInstance.get(
     `${API_ROOT}/v1/transactions/get-all-transactions`
