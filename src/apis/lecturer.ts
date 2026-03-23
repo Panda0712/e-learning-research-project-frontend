70635cf4ea4cd144c375d3f4317892a7866e310c
Phạm Ngũ Tuân <123355012+Panda0712@users.noreply.github.com>
2026-03-23 22:37:26 +0700

    Update lecturer.ts

--- src/apis/lecturer.ts
+++ src/apis/lecturer.ts
@@ -1,7 +1,6 @@
 import authorizedAxiosInstance from "../utils/authorizedAxios";
 import { API_ROOT } from "../utils/constants";
 
-// Gọi API lấy danh sách học viên của giảng viên
 const getMyStudentsAPI = async (lecturerId: number) => {
   const res = await authorizedAxiosInstance.get(
     `${API_ROOT}/v1/enrollments/lecturer/${lecturerId}/students`
@@ -9,7 +8,6 @@ const getMyStudentsAPI = async (lecturerId: number) => {
 
   return res.data;
 };
-
 
 export const lecturerService = {
   getMyStudentsAPI,
