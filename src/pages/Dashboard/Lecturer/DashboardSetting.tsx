fe807161eca036790048badbbe022bfb8f6629bb
Phạm Ngũ Tuân <123355012+Panda0712@users.noreply.github.com>
2026-03-23 22:48:44 +0700

    Update DashboardSetting.tsx

--- src/pages/Dashboard/Lecturer/DashboardSetting.tsx
+++ src/pages/Dashboard/Lecturer/DashboardSetting.tsx
@@ -1,13 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useEffect } from "react";
 import SettingsMenu from "../../../components/dashboard/lecturer/setting/SettingMenu";
 import AccountSetting from "../../../components/dashboard/lecturer/setting/AccountSetting";
 import PayoutDetail from "../../../components/dashboard/lecturer/setting/PayoutDetail";
 import NotificationSetting from "../../../components/dashboard/lecturer/setting/NotificationSetting";
 import { profileService } from "../../../apis/profile";
+import { toast } from "react-toastify";
 
 const DashboardSetting = () => {
   const [activeTab, setActiveTab] = useState("account");
-  const [userProfile, setUserProfile] = useState<{name: string, avatar: string} | undefined>();
+  const [userProfile, setUserProfile] = useState<
+    { name: string; avatar: string } | undefined
+  >();
 
   useEffect(() => {
     const fetchProfileForMenu = async () => {
@@ -15,10 +19,10 @@ const DashboardSetting = () => {
         const data = await profileService.getUserFullProfileAPI();
         setUserProfile({
           name: data.name,
-          avatar: data.avatar
+          avatar: data.avatar,
         });
-      } catch (error) {
-        console.error("Lỗi lấy profile cho Menu:", error);
+      } catch (error: any) {
+        toast.error(error?.message || "Failed to load profile data!");
       }
     };
     fetchProfileForMenu();
@@ -29,10 +33,10 @@ const DashboardSetting = () => {
       <h1 className="text-2xl font-bold mb-6 text-gray-800">Setting</h1>
 
       <div className="flex flex-col lg:flex-row gap-8">
-        <SettingsMenu 
-          activeTab={activeTab} 
-          setActiveTab={setActiveTab} 
-          userProfile={userProfile} 
+        <SettingsMenu
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+          userProfile={userProfile}
         />
 
         <div className="w-full lg:w-3/4 bg-white rounded-xl shadow-sm p-8">
