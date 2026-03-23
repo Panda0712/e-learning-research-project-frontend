def1b660048072c9962f8d056572313f305ba543
Phạm Ngũ Tuân <123355012+Panda0712@users.noreply.github.com>
2026-03-23 23:10:59 +0700

    Update notification.ts

--- src/apis/notification.ts
+++ src/apis/notification.ts
@@ -2,12 +2,12 @@ import authorizedAxiosInstance from '../utils/authorizedAxios';
 import { API_ROOT } from '../utils/constants';
 
 const getNotificationSettingsAPI = async () => {
-  const response = await authorizedAxiosInstance.get(`${API_ROOT}/v1/users/notification-settings`);// nhớ sửa lại đường dẫn
+  const response = await authorizedAxiosInstance.get(`${API_ROOT}/v1/users/notification-settings`);
   return response.data;
 };
 
 const updateNotificationSettingAPI = async (data: Record<string, boolean>) => {
-  const response = await authorizedAxiosInstance.put(`${API_ROOT}/v1/users/notification-settings`, data);// nhớ sửa lại đường dẫn
+  const response = await authorizedAxiosInstance.put(`${API_ROOT}/v1/users/notification-settings`, data);
   return response.data;
 };
 
